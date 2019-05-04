c962e727e8599dc4d675782e3adb8f6ba5ac2363
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-05-04 23:14:05 +0200

    refactoring of resolvers

--- akka-serialization-jackson/src/main/scala/akka/serialization/jackson/ActorRefModule.scala
+++ akka-serialization-jackson/src/main/scala/akka/serialization/jackson/ActorRefModule.scala
@@ -9,16 +9,17 @@ import akka.annotation.InternalApi
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonTokenId
-import com.fasterxml.jackson.databind._
-import com.fasterxml.jackson.databind.deser.Deserializers
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer
-import com.fasterxml.jackson.databind.ser.Serializers
 import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer
 
 /**
  * INTERNAL API: Adds support for serializing and deserializing [[ActorRef]].
  */
-@InternalApi private[akka] trait ActorRefModule extends ActorRefSerializerModule with ActorRefDeserializerModule
+@InternalApi private[akka] trait ActorRefModule extends JacksonModule {
+  addSerializer(classOf[ActorRef], () => ActorRefSerializer.instance, () => ActorRefDeserializer.instance)
+}
 
 /**
  * INTERNAL API
@@ -36,35 +37,6 @@ import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer
   override def serialize(value: ActorRef, jgen: JsonGenerator, provider: SerializerProvider) {
     val serializedActorRef = value.path.toSerializationFormatWithAddress(currentSystem().provider.getDefaultAddress)
     jgen.writeString(serializedActorRef)
-  }
-}
-
-/**
- * INTERNAL API
- */
-@InternalApi private[akka] object ActorRefSerializerResolver extends Serializers.Base {
-
-  private val ActorRefClass = classOf[ActorRef]
-
-  override def findSerializer(
-      config: SerializationConfig,
-      javaType: JavaType,
-      beanDesc: BeanDescription): JsonSerializer[_] = {
-    val cls = javaType.getRawClass
-    if (ActorRefClass.isAssignableFrom(cls))
-      ActorRefSerializer.instance
-    else
-      super.findSerializer(config, javaType, beanDesc)
-  }
-
-}
-
-/**
- * INTERNAL API: Adds serialization support for ActorRef.
- */
-@InternalApi private[akka] trait ActorRefSerializerModule extends JacksonModule {
-  this += { ctx =>
-    ctx.addSerializers(ActorRefSerializerResolver)
   }
 }
 
@@ -89,31 +61,4 @@ import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer
     } else
       ctxt.handleUnexpectedToken(handledType(), jp).asInstanceOf[ActorRef]
   }
-}
-
-/**
- * INTERNAL API
- */
-@InternalApi private[akka] object ActorRefDeserializerResolver extends Deserializers.Base {
-
-  private val ActorRefClass = classOf[ActorRef]
-
-  override def findBeanDeserializer(
-      javaType: JavaType,
-      config: DeserializationConfig,
-      beanDesc: BeanDescription): JsonDeserializer[_] = {
-    val cls = javaType.getRawClass
-    if (ActorRefClass.isAssignableFrom(cls))
-      ActorRefDeserializer.instance
-    else
-      super.findBeanDeserializer(javaType, config, beanDesc)
-  }
-
-}
-
-/**
- * INTERNAL API: Adds deserialization support for ActorRef.
- */
-@InternalApi private[akka] trait ActorRefDeserializerModule extends JacksonModule {
-  this += ActorRefDeserializerResolver
 }
--- akka-serialization-jackson/src/main/scala/akka/serialization/jackson/JacksonModule.scala
+++ akka-serialization-jackson/src/main/scala/akka/serialization/jackson/JacksonModule.scala
@@ -7,8 +7,14 @@ package akka.serialization.jackson
 import akka.annotation.InternalApi
 import com.fasterxml.jackson.core.Version
 import com.fasterxml.jackson.core.util.VersionUtil
+import com.fasterxml.jackson.databind.BeanDescription
+import com.fasterxml.jackson.databind.DeserializationConfig
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.databind.Module.SetupContext
+import com.fasterxml.jackson.databind.SerializationConfig
 import com.fasterxml.jackson.databind.`type`.TypeModifier
 import com.fasterxml.jackson.databind.deser.Deserializers
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier
@@ -25,6 +31,34 @@ import com.fasterxml.jackson.databind.ser.Serializers
     val version = akka.Version.current
     VersionUtil.parseVersion(version, groupId, artifactId)
   }
+
+  class SerializerResolverByClass(clazz: Class[_], deserializer: () => JsonSerializer[_]) extends Serializers.Base {
+
+    override def findSerializer(
+        config: SerializationConfig,
+        javaType: JavaType,
+        beanDesc: BeanDescription): JsonSerializer[_] = {
+      if (clazz.isAssignableFrom(javaType.getRawClass))
+        deserializer()
+      else
+        super.findSerializer(config, javaType, beanDesc)
+    }
+
+  }
+
+  class DeserializerResolverByClass(clazz: Class[_], serializer: () => JsonDeserializer[_]) extends Deserializers.Base {
+
+    override def findBeanDeserializer(
+        javaType: JavaType,
+        config: DeserializationConfig,
+        beanDesc: BeanDescription): JsonDeserializer[_] = {
+      if (clazz.isAssignableFrom(javaType.getRawClass))
+        serializer()
+      else
+        super.findBeanDeserializer(javaType, config, beanDesc)
+    }
+
+  }
 }
 
 /**
@@ -38,6 +72,7 @@ import com.fasterxml.jackson.databind.ser.Serializers
  * INTERNAL API
  */
 @InternalApi private[akka] trait JacksonModule extends Module {
+  import JacksonModule._
 
   private val initializers = Seq.newBuilder[SetupContext => Unit]
 
@@ -45,6 +80,16 @@ import com.fasterxml.jackson.databind.ser.Serializers
 
   def setupModule(context: SetupContext) {
     initializers.result().foreach(_.apply(context))
+  }
+
+  def addSerializer(
+      clazz: Class[_],
+      serializer: () => JsonSerializer[_],
+      deserializer: () => JsonDeserializer[_]): this.type = {
+    this += { ctx =>
+      ctx.addSerializers(new SerializerResolverByClass(clazz, serializer))
+      ctx.addDeserializers(new DeserializerResolverByClass(clazz, deserializer))
+    }
   }
 
   protected def +=(init: SetupContext => Unit): this.type = { initializers += init; this }
